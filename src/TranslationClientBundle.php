e59b9393fc000199aa4cdb3fce44316016966739
maoxtrem <maoxtrem@gmail.com>
2026-04-11 21:28:35 -0500

    ajustes

--- src/TranslationClientBundle.php
+++ src/TranslationClientBundle.php
@@ -1,6 +1,19 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Maoxtrem\TranslationClientKit;
+
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class TranslationClientBundle extends Bundle {}
+final class TranslationClientBundle extends Bundle
+{
+    /**
+     * Obligamos a Symfony a subir un nivel para encontrar 
+     * las carpetas /config y /public.
+     */
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
